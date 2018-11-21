85887aebe062f654ef11b5df3e7652600b094eb9
Curso fisi4028  je.forero 2018-09-26 <fisi4028@clustergate.local>
2018-11-21 06:36:57 -0500

    torque script added, all running smoothly

---
+++ m.neira10_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=4,mem=1gb,walltime=00:10:00
+#PBS -M m.neira10@uniandes.edu.co
+#PBS -m abe
+#PBS -N mneira10_ejer29
+#PBS -j oe
+
+
+module load anaconda/python3 
+module load gcc/4.9.4 
+module load openmpi/1.8.5
+cd /hpcfs/home/fisi4028/m.neira10/MauricioNeira_Ejercicio29
+cd helloWorld
+make
+cd ..
+cd adveccion
+make 
